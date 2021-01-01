43404fac72f88b13758f99d54951f073c836ce9c
javalo <lo.cheikhna@gmail.com>
2021-01-01 17:18:35 +0100

    Connect our custom class FightInfoCell with our View xml which is flight_cell.xml

--- app/src/main/java/com/example/fight_tracking/FlightInfoCell.kt
+++ app/src/main/java/com/example/fight_tracking/FlightInfoCell.kt
@@ -8,6 +8,26 @@ import android.widget.TextView
 
 class FlightInfoCell : LinearLayout {
 
+
+    var flightName: TextView? = null
+        private set
+    var depAirportName: TextView? = null
+        private set
+    var arrAirportName: TextView? = null
+        private set
+    var flightTime: TextView? = null
+        private set
+    var depDate: TextView? = null
+        private set
+    var arrDate: TextView? = null
+        private set
+    var depHour: TextView? = null
+        private set
+    var arrHour: TextView? = null
+        private set
+
+
+
     constructor(context: Context?) : super(context) {
         initLayout()
     }
@@ -24,8 +44,19 @@ class FlightInfoCell : LinearLayout {
         initLayout()
     }
 
+    private fun bindViews() {
+        flightName = findViewById(R.id.flight_name)
+        depAirportName = findViewById(R.id.departure_airport)
+        arrAirportName = findViewById(R.id.arrival_airport)
+        flightTime = findViewById(R.id.flight_duration)
+        depDate = findViewById(R.id.departure_date)
+        arrDate = findViewById(R.id.arrival_date)
+        depHour = findViewById(R.id.departure_hour)
+        arrHour = findViewById(R.id.arrival_hour)
+    }
 
     private fun initLayout() {
-
+        LayoutInflater.from(context).inflate(R.layout.flight_cell, this, true)
+        bindViews()
     }
 }
